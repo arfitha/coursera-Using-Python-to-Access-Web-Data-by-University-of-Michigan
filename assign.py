9554d385c3930f14ae5bac4aec5149b075ee7951
V V Arfitha <72938978+arfitha@users.noreply.github.com>
2020-10-19 22:50:55 +0530

    Add files via upload
    Scraping Numbers from HTML using BeautifulSoup In this assignment you will write a Python program similar to http://www.py4e.com/code3/urllink2.py. The program will use urllib to read the HTML from the data files below, and parse the data, extracting numbers and compute the sum of the numbers in the file.
    
    We provide two files for this assignment. One is a sample file where we give you the sum for your testing and the other is the actual data you need to process for the assignment.
    
    Sample data: http://py4e-data.dr-chuck.net/comments_42.html (Sum=2553)
    Actual data: http://py4e-data.dr-chuck.net/comments_696697.html (Sum ends with 42)
    You do not need to save these files to your folder since your program will read the data directly from the URL. Note: Each student will have a distinct data url for the assignment - so only use your own data url for analysis.

---
+++ assign.py
@@ -0,0 +1,8 @@
+import urllib.request
+import xml.etree.ElementTree as ET
+
+url = 'http://py4e-data.dr-chuck.net/comments_696699.xml'
+response = urllib.request.urlopen(url)
+tree = ET.fromstring(response.read())
+total = sum([int(count.text) for count in tree.findall('comments/comment/count')])
+print(total)
